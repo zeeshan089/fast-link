987b6a6f84049a7bbac2ef750be85184dc37499b
Zeeshan Ali <zeeshanali0089@gmail.com>
2025-08-25 17:13:20 +0530

    Minor bug fix

--- app/main.py
+++ app/main.py
@@ -6,6 +6,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Value returned from root. First Success!"}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
