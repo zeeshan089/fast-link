eec6af54b969f0b9d36383064479fb43c3dfb04f
Zeeshan Ali <zeeshanali0089@gmail.com>
2025-08-25 09:29:01 +0530

    first push of FastAPI code

---
+++ app/main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Value returned from root"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
